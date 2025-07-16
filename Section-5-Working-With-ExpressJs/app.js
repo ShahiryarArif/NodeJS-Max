8144909d9df699cfddbad23810013215f8ca29aa
Shahiryar Arif <shahiryararif@MacBook-Pro.local>
2025-07-16 20:11:18 +0500

    62. How Middleware Works

---
+++ Section-5-Working-With-ExpressJs/app.js
@@ -0,0 +1,19 @@
+import http from "http";
+
+import express from "express";
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("In the middleware");
+  next();
+})
+
+app.use((req, res, next) => {
+  console.log("In another middleware");
+  res.send("<h1>hello from the other side</h1>");
+})
+
+const server = http.createServer(app);
+
+server.listen(3000);
