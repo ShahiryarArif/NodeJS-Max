4b785e54e2160404a241a8bb2c68a2afb7c84405
Shahiryar Arif <shahiryararif@MacBook-Pro.local>
2025-07-17 16:31:51 +0500

    65. Parsing Incoming Request

--- Section-5-Working-With-ExpressJs/app.js
+++ Section-5-Working-With-ExpressJs/app.js
@@ -1,19 +1,27 @@
-import http from "http";
-
+import bodyParser from "body-parser";
 import express from "express";
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use((req, res, next) => {
   console.log("In the middleware");
   next();
 })
 
-app.use((req, res, next) => {
+app.use("/add-product", (req, res, next) => {
+  res.send("<form action='/product' method='POST'><input type='text' name='title'/><button type='submit'>Submit</button></form>")
+})
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+})
+
+app.use("/",(req, res, next) => {
   console.log("In another middleware");
   res.send("<h1>hello from the other side</h1>");
 })
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
