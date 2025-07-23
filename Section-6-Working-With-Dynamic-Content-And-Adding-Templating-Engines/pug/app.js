3425fe0dc370949572987dd51b4854802e75c2f7
Shahiryar Arif <shahiryararif@MacBook-Pro.local>
2025-07-24 02:09:00 +0500

    85. Converting HTML Files to Pug

--- Section-6-Working-With-Dynamic-Content-And-Adding-Templating-Engines/pug/app.js
+++ Section-6-Working-With-Dynamic-Content-And-Adding-Templating-Engines/pug/app.js
@@ -5,6 +5,10 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set("view engine", "pug");
+
+app.set("views", "views");
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
@@ -15,7 +19,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render("404.pug");
 });
 
 app.listen(3000);
