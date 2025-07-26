068333e504287617d550993ad208cd1ac4a065c9
Shahiryar Arif <shahiryararif@MacBook-Pro.local>
2025-07-26 19:04:21 +0500

    93. Working on the Layout with Partials

--- Section-6-Working-With-Dynamic-Content-And-Adding-Templating-Engines/ejs/app.js
+++ Section-6-Working-With-Dynamic-Content-And-Adding-Templating-Engines/ejs/app.js
@@ -5,7 +5,10 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require('./routes/admin').router;
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -15,7 +18,9 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', {
+        pageTitle: 'Page Not Found',
+    });
 });
 
 app.listen(3000);
