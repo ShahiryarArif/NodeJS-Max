6a92bead9f013868ce9adc78ca87326631b25542
Shahiryar Arif <shahiryararif@MacBook-Pro.local>
2025-08-25 17:01:13 +0500

    Initial mongoose setup

---
+++ Section-13-Working-with-Mongoose/app.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const { default: mongoose } = require('mongoose');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findById(1)
+    .then(user => {
+      req.user = new User(user.username, user.email, user.cart, user._id);
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoose.connect('mongodb+srv://shahiryararif:12345@cluster0.y7gpodz.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0')
+  .then(result => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
