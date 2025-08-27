5269d643de6d3c46e08a18099a39250e7f242bf3
Shahiryar Arif <shahiryararif@MacBook-Pro.local>
2025-08-28 02:58:29 +0500

    Initial commit

---
+++ Section-14-Sessions-And-Cookies/routes/auth.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const authController = require('../controllers/auth');
+
+const router = express.Router();
+
+// /admin/add-product => GET
+router.get('/login', authController.getLogin);
+
+router.post('/login', authController.postLogin);
+
+module.exports = router;
