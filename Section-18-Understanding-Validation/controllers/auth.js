27d5023e7d208fd1ee2cdbbef88d245147418122
Shahiryar Arif <shahiryararif@MacBook-Pro.local>
2025-09-03 01:38:37 +0500

    298. Adding Async Validation

--- Section-18-Understanding-Validation/controllers/auth.js
+++ Section-18-Understanding-Validation/controllers/auth.js
@@ -1,6 +1,7 @@
 const bscrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
+const { validationResult } = require('express-validator/check');
 
 const User = require('../models/user');
 
@@ -57,34 +58,36 @@ exports.postLogin = (req, res, next) => {
 
 exports.postSignup = (req, res, next) => {
   const { email, password, confirmPassword } = req.body;
-  User.findOne({ email: email })
-    .then(userDoc => {
-      if (userDoc) {
-        req.flash('error', 'E-Mail exists already, please pick a different one.');
-        return res.redirect('/signup');
-      }
-      return bscrypt
-      .hash(password, 12)
-      .then(hashedPassword => {
-        const user = new User({
-          email: email,
-          password: hashedPassword,
-          cart: { items: [] }
-        });
-        return user.save();
-      })
-      .then(result => {
-        res.redirect('/login');
-        return transporter.sendMail({
-          to: email,
-          from: 'shop@node-complete.com',
-          subject: 'Signup succeeded!',
-          html: '<h1>You successfully signed up!</h1>'
-        });
-      })
-      .catch(err => console.log(err));
-    })
-    .catch(err => console.log(err));
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422)
+            .render('auth/signup', {
+              path: '/signup',
+              pageTitle: 'Signup',
+              errorMessage: errors.array()[0].msg
+            });
+  }
+
+  bscrypt
+  .hash(password, 12)
+  .then(hashedPassword => {
+    const user = new User({
+      email: email,
+      password: hashedPassword,
+      cart: { items: [] }
+    });
+    return user.save();
+  })
+  .then(result => {
+    res.redirect('/login');
+    return transporter.sendMail({
+      to: email,
+      from: 'shop@node-complete.com',
+      subject: 'Signup succeeded!',
+      html: '<h1>You successfully signed up!</h1>'
+    });
+  })
+  .catch(err => console.log(err));
 
 };
 
