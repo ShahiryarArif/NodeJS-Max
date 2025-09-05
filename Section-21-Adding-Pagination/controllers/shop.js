c4ee34c96b6e4d4bfc5acb399151a00affc69e7d
Shahiryar Arif <shahiryararif@MacBook-Pro.local>
2025-09-05 13:18:23 +0500

    345. Re-Using the Pagination Logic & Controls

--- Section-21-Adding-Pagination/controllers/shop.js
+++ Section-21-Adding-Pagination/controllers/shop.js
@@ -8,13 +8,27 @@ const Order = require('../models/order');
 const ITEMS_PER_PAGE = 2;
 
 exports.getProducts = (req, res, next) => {
+  const currentPage = +req.query.page || 1;
+  let totalItems;
   Product.find()
+    .countDocuments()
+    .then(count => {
+      totalItems = count;
+      return Product.find()
+        .skip((currentPage - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then(products => {
-      console.log(products);
       res.render('shop/product-list', {
         prods: products,
         pageTitle: 'All Products',
         path: '/products',
+        currentPage: currentPage,
+        hasNextPage: ITEMS_PER_PAGE * currentPage < totalItems,
+        hasPreviousPage: currentPage > 1,
+        nextPage: currentPage + 1,
+        previousPage: currentPage - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
       });
     })
     .catch(err => {
