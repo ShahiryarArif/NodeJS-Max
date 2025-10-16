b44307403578827f91656a1bae3491213b7af49e
Shahiryar Arif <shahiryararif@MacBook-Pro.local>
2025-10-16 14:24:06 +0500

    392. Logging In & Creating JSON Web Tokens (JWTs)

---
+++ Section-25-Working-with-REST-APIs-The-Practical-Application/sending-post-requests/controllers/auth.js
@@ -0,0 +1,64 @@
+const User = require('../models/user');
+const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+exports.signup = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed!');
+    error.statusCode = 422;
+    error.data = errors.array();
+    throw error;
+  }
+
+  const { name, email, password } = req.body;
+
+  bcrypt
+    .hash(password, 12)
+    .then(hashedPw => {
+      const user = new User({
+        name: name,
+        email: email,
+        password: hashedPw,
+        posts: []
+      });
+      return user.save();
+    })
+    .then(result => {
+      res.status(201).json({ message: 'User created!', userId: result._id });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+  let loadedUser;
+  User.findOne({ email: email })
+    .then(user => {
+      if (!user) {
+        const error = new Error('A user with this email could not be found.');
+        error.statusCode = 401;
+        throw error;
+      }
+      loadedUser = user;
+      return bcrypt.compare(password, user.password);
+    })
+    .then(isEqual => {
+      if (!isEqual) {
+        const error = new Error('Wrong password!');
+        error.statusCode = 401;
+        throw error;
+      }
+      res.status(200).json({ message: 'Logged in!', userId: loadedUser._id });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
